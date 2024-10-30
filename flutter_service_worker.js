2881b9997fe9b3d0afda3278d81eff3a39fe3727
Santiago Vasquez <santivaz852@hotmail.com>
2024-10-30 18:21:08 -0500

    second

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "abfad626e81eb9d42464842a760291c9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "45503a18a9a42304a3de0fcfbd038423",
+"flutter_bootstrap.js": "f221a377e1c385fde1bf5a44db6c1997",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
