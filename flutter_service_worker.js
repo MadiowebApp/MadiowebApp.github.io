944c4b70183bac1aff6bc1aff52d43923db97327
Santiago Vasquez <santivaz852@hotmail.com>
2025-01-24 20:19:10 -0500

    Solving gradient issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "dbd687901817a6cd315ac823b680a90c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d55bc94358c554a87c3b1cfaa645fa68",
+"flutter_bootstrap.js": "29d834626473963218ba6fd455a49bd8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "07d50d3f7709c5925d3909bca43a0c7b",
 "/": "07d50d3f7709c5925d3909bca43a0c7b",
-"main.dart.js": "5923b994910f30cd8674f6e1d5cd7fad",
+"main.dart.js": "0cc72437378f7f70debe6e442efa1531",
 "manifest.json": "87bb1213c2304081707ee17adb2be2b2",
 "version.json": "669c96c637a4d248efe7cf845bed2320"};
 // The application shell files that are downloaded before a service worker can
