d8a1bc85aa6cd60d8901998680808245eebf80c0
Santiago Vasquez <santivaz852@hotmail.com>
2025-05-20 19:58:38 -0500

    Products Images V2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2631301121"
+    serviceWorkerVersion: "2689383771"
   }
 });
