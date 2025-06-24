4edef092a0f8baf70f2798b1ebca3f9059a66fd1
Santiago Vasquez <santivaz852@hotmail.com>
2025-06-23 21:12:24 -0500

    Categories, subcategories and Products complete

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2600167245"
+    serviceWorkerVersion: "3419709684"
   }
 });
