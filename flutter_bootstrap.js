42e7a0a5c6fb3968c50f18d496d0dce0ce4c1337
Santiago Vasquez <santivaz852@hotmail.com>
2025-07-09 21:50:51 -0500

    Order details copy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3397723751"
+    serviceWorkerVersion: "1623650108"
   }
 });
