7debe5a62a23bf745bf028a773ef6d5eca73de9b
Santiago Vasquez <santivaz852@hotmail.com>
2025-07-31 20:56:55 -0500

    Oder stepper Flow and automated Whatsapp messages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1880933688"
+    serviceWorkerVersion: "2443427276"
   }
 });
