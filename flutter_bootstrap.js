9e905517f58d0a857273517a98f33ff3ce02babd
Santiago Vasquez <santivaz852@hotmail.com>
2025-08-05 19:24:30 -0500

    Create new Cellars and Suppliers

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3689189521"
+    serviceWorkerVersion: "2999942200"
   }
 });
