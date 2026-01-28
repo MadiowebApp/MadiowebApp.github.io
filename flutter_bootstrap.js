90698098b81ec397bd1115ab1300091f72c7a7b7
Santiago Vasquez <santivaz852@hotmail.com>
2026-01-28 13:34:57 -0500

    simplified product screen for big screens

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2241602151"
+    serviceWorkerVersion: "4231780037"
   }
 });
