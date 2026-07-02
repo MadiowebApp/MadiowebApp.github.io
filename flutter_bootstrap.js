e2d7b7cd45a6fe41a8b03c81c35e42c73c1cde6e
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-01 21:30:46 -0500

    deploy: 1.2.7+12 - version bump (versionCode 11 already used in Play)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1739611316" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2713190046" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
