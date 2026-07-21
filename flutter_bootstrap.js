e6ad5961d1061a0f85eef4e78c12f5a0c8439ea0
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-21 10:04:48 -0500

    deploy: descuentos por tienda + mute de correos por tienda (1.2.11)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2220517670" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1529946590" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
