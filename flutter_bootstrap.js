6e8f728232f49dd0f4fef756a3d32175bbfbe0f3
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-27 13:01:39 -0500

    deploy: identidad Madio en menu y dashboard + selector de tiendas (2.0.1+17)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3610916814" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2046295688" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
