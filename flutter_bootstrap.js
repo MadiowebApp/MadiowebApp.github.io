ee177832aabe106e13ce1f8e7b30f2c352ff54ad
Santiago Vasquez <santivaz852@hotmail.com>
2026-09-02 21:37:10 -0500

    deploy: rediseño de la welcome screen (IndexPageWhiteBackground, Montserrat)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1304139631" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1556757950" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
